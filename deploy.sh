6775cd78655565fefc8bf21e9299bdaeeb89b2c3
Pulkit Kathuria <kevincobain2000@gmail.com>
2019-08-13 21:36:18 +0900

    updated docs

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-git push -f git@github.com:kevincobain2000/golang-cheet-sheet.git master:gh-pages
+git push -f git@github.com:kevincobain2000/golang-cheat-sheet.git master:gh-pages
 
 cd -
